5804dc499321e284d2b2ba5d07cecc05ac995956
Ahmad <wisamfaithful@gmail.com>
2015-02-07 01:23:17 -0500

    implemented push method

--- code.rb
+++ code.rb
@@ -17,6 +17,16 @@ def print_values(list_node)
   end
 end
 
+def reverse_list(list)
+  stack = Stack.new
+
+  while list
+    stack.push(list)
+    list = list.next_node
+  end
+  stack.data
+end
+
 
 class Stack
   attr_reader :data
@@ -27,15 +37,28 @@ class Stack
 
   # Push an item onto the stack
   def push(element)
-    # IMPLEMENT ME!
+      if @data == nil 
+        #if @data is empty, then push a new node with the
+        #element value and make it point to nil because it will be the
+        #last node
+        temp = LinkedListNode.new(element.value, nil) 
+        @data = temp
+      else
+        #otherwise push a new node but make it point to the
+        #last pushed node which is @data
+        temp = @data
+        temp2 = LinkedListNode.new(element.value, temp)
+        #make @data equal to the last pushed node
+        @data = temp2
+      end
   end
 
-    # Pop an item off the stack.  
-    # Remove the last item that was pushed onto the
-    # stack and return it to the user
-    def pop
-        # IMPLEMENT ME
-    end
+  # Pop an item off the stack.  
+  # Remove the last item that was pushed onto the
+  # stack and return it to the user
+  def pop
+    
+  end
 
 end
 
@@ -43,5 +66,13 @@ node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
 node3 = LinkedListNode.new(12, node2)
 
+
 print_values(node3)
+
+puts "-------"
+
+revlist = reverse_list(node3)
+
+print_values(revlist)
+
 
